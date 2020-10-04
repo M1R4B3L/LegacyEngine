d5ec326ee5c338451ca121ccce44dfd4bf27c8de
bottzo <45202057+bottzo@users.noreply.github.com>
2020-10-04 21:47:43 +0200

    added the imgui library (crashes) + renamed solution and project

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -2,7 +2,7 @@
 #include "Application.h"
 #include "Globals.h"
 
-#include "SDL/include/SDL.h"
+#include "SDL.h"
 #pragma comment( lib, "Source/Dependencies/SDL/libx86/SDL2.lib" )
 #pragma comment( lib, "Source/Dependencies/SDL/libx86/SDL2main.lib" )
 
