abdb3cd7aa1a4dfa28ccba8513ac2686ca9bfea3
bottzo <45202057+bottzo@users.noreply.github.com>
2020-10-07 12:33:01 +0200

    rendering imgui above the plane grid

--- Source/ModuleSceneIntro.cpp
+++ Source/ModuleSceneIntro.cpp
@@ -1,7 +1,11 @@
 #include "Globals.h"
+#include "GL/glew.h"
 #include "Application.h"
 #include "ModuleSceneIntro.h"
 #include "Primitive.h"
+#include "imgui.h"
+#include "examples\imgui_impl_sdl.h"
+#include "examples\imgui_impl_opengl3.h"
 
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -33,6 +37,32 @@ bool ModuleSceneIntro::CleanUp()
 // Update
 update_status ModuleSceneIntro::Update(float dt)
 {
+	//IMGUI!!!!
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplSDL2_NewFrame(App->window->window);
+	ImGui::NewFrame();
+	bool showdemowindow = true;
+	ImGui::ShowDemoWindow(&showdemowindow);
+	ImGui::Render();
+	ImGuiIO& io = ImGui::GetIO();
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+	//glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+	//glClear(GL_COLOR_BUFFER_BIT);
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+	// Update and Render additional Platform Windows
+	// (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+	//  For this specific demo app we could also call SDL_GL_MakeCurrent(window, gl_context) directly)
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		SDL_Window* backup_current_window = SDL_GL_GetCurrentWindow();
+		SDL_GLContext backup_current_context = SDL_GL_GetCurrentContext();
+		ImGui::UpdatePlatformWindows();
+		ImGui::RenderPlatformWindowsDefault();
+		SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
+	}
+	
 	Plane p(0, 1, 0, 0);
 	p.axis = true;
 	p.Render();
