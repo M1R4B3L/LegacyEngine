418c38e1f0b0a81a261c7cddcecd8d67a8555448
bottzo <45202057+bottzo@users.noreply.github.com>
2020-12-05 16:06:41 +0100

    added the new math geolib

--- Source/Dependencies/MathGeoLib/include/Math/float3.h
+++ Source/Dependencies/MathGeoLib/include/Math/float3.h
@@ -36,11 +36,10 @@
 #ifdef MATH_OGRE_INTEROP
 #include <OgreVector3.h>
 #endif
-/*
+
 #ifdef MATH_BULLET_INTEROP
-#include "../../../Bullet/include/LinearMath/btVector3.h"
+#include "LinearMath/btVector3.h"
 #endif
-*/
 
 MATH_BEGIN_NAMESPACE
 
@@ -283,7 +282,6 @@ public:
 	/// Sets all elements of this vector.
 	/** @see x, y, z, At(). */
 	void Set(float x, float y, float z);
-	void Set(float* v);
 
 	/// Converts the given vector represented in spherical coordinates to an euclidean float3 (x,y,z) triplet.
 	/** @param azimuth The direction, or yaw, of the vector. This function uses the convention that the X-Z plane is
@@ -689,12 +687,10 @@ public:
 	static float3 FromQVector3D(const QVector3D &v) { return (float3)v; }
 	static float3 FromString(const QString &str) { return FromString(str.toStdString()); }
 #endif
-	/*
 #ifdef MATH_BULLET_INTEROP
 	float3(const btVector3 &other) { x = other.x(); y = other.y(); z = other.z(); }
 	operator btVector3() const { return btVector3(x, y, z); }
 #endif
-*/
 };
 
 /// Prints this float3 to the given stream.
