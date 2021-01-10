2ff6097c05513779881df096ef2778af5bd85ce0
M1R4B3L <amirabelmartinez@gmail.com>
2021-01-10 22:33:44 +0100

    photos and set scripting we are so bad at math

--- Engine/ScriptRequirements/ScriptTemplate.cpp
+++ Engine/ScriptRequirements/ScriptTemplate.cpp
@@ -2,6 +2,7 @@
 
 Input input;
 
+
 float speed = 50.0f;
 float angle = 0.0f;
 float velocity = 0.0f;
@@ -16,37 +17,45 @@ extern "C" __declspec(dllexport) void Update(GameObject * object) {
 	ComponentTransform* transform = (ComponentTransform*)object->GetComponent(ComponentType::Transform);
 	float3 translation; Quat rotation; float3 scale;
 	if (transform != nullptr) {
+
 		transform->GetLocalTransform(translation, scale, rotation);
 
-		float3 forward = float3((rotation.x * rotation.z + rotation.w * rotation.y),(rotation.y * rotation.z - rotation.w * rotation.x), (rotation.x * rotation.x + rotation.y * rotation.y));
+		float4x4 inverse = transform->GetGlobalTransform();
+
+		float3 colum = inverse.Col3(2);
+		float3 forward = colum.Normalized();
 
 		if (input.GetKeyRepeat(SDL_SCANCODE_UP)) {
 		
-			velocity -= speed *GetGameTime();	
-			//ConsoleLog("%f", translation.z);
-			transform->SetLocalTransform(forward * velocity, scale, rotation);
+			velocity += speed *GetGameTime();	
+
 			ConsoleLog(std::to_string(GetGameTime()).c_str());
 			ConsoleLog("Moving Up!");
 		}
 		if (input.GetKeyRepeat(SDL_SCANCODE_DOWN)) {
 		
-			velocity += speed * GetGameTime();
-			transform->SetLocalTransform(forward * velocity, scale, rotation);
+			velocity -= speed * GetGameTime();
+			
 			ConsoleLog("Moving Down!");
 		}
 		if (input.GetKeyRepeat(SDL_SCANCODE_RIGHT)) {
 			
 			angle -= speed * GetGameTime();
-			transform->SetLocalTransform(translation, scale, Quat::FromEulerXYX(0, angle * 0.01745329252, 0));
+
+			rotation = rotation.RotateY(angle * 0.01745329252);
+
 			ConsoleLog("Moving Right!");
 		}
 		if (input.GetKeyRepeat(SDL_SCANCODE_LEFT)) {
 		
 			angle += speed * GetGameTime();
-			transform->SetLocalTransform(translation, scale, Quat::FromEulerXYX(0, angle * 0.01745329252, 0));
+
+			rotation = rotation.RotateY(angle * 0.01745329252);
+	
 			ConsoleLog("Moving Left!");	
 		}
 
+		transform->SetLocalTransform(forward*velocity, scale, rotation);
 	}
 }
 
