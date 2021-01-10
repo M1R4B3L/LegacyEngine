a02d569a92f66cdca455142054e8763e0530bc15
M1R4B3L <amirabelmartinez@gmail.com>
2021-01-10 20:02:06 +0100

    We can move the tank

--- Engine/ScriptRequirements/ScriptTemplate.cpp
+++ Engine/ScriptRequirements/ScriptTemplate.cpp
@@ -3,34 +3,50 @@
 Input input;
 
 float speed = 3.0f;
+float angle = 0.0f;
+float velocity = 0.0f;
 
 extern "C" __declspec(dllexport) void Start(GameObject* object) {
 	ConsoleLog("Hi Scripter");
 	
 }
 
-extern "C" __declspec(dllexport) void Update(GameObject* object) {
+extern "C" __declspec(dllexport) void Update(GameObject * object) {
 
-	if (input.GetKeyRepeat(SDL_SCANCODE_UP)) {
-		ComponentTransform* transform = (ComponentTransform*)object->GetComponent(ComponentType::Transform);
-		if (transform != nullptr) {
-			float3 translation; Quat rotation; float3 scale;
-			transform->GetLocalTransform(translation, scale, rotation);
-			translation.z += speed * GetGameTime();
-			transform->SetLocalTransform(translation, scale, rotation);
-			ConsoleLog("%f", translation.z);
-			ConsoleLog("Moving Up!!!!!!!");
+	ComponentTransform* transform = (ComponentTransform*)object->GetComponent(ComponentType::Transform);
+	float3 translation; Quat rotation; float3 scale;
+	if (transform != nullptr) {
+		transform->GetLocalTransform(translation, scale, rotation);
+
+		float3 forward = float3((rotation.x * rotation.z + rotation.w * rotation.y),(rotation.y * rotation.z - rotation.w * rotation.x), (rotation.x * rotation.x + rotation.y * rotation.y));
+
+		if (input.GetKeyRepeat(SDL_SCANCODE_UP)) {
+		
+			velocity += speed*GetGameTime();	
+			//ConsoleLog("%f", translation.z);
+			transform->SetLocalTransform(forward * velocity, scale, rotation);
+			ConsoleLog("Moving Up!");
 		}
-	}
-	/*else if (input.GetKeyRepeat(SDL_SCANCODE_DOWN)) {
+		if (input.GetKeyRepeat(SDL_SCANCODE_DOWN)) {
+		
+			velocity -= speed * GetGameTime();
+			transform->SetLocalTransform(forward * velocity, scale, rotation);
+			ConsoleLog("Moving Down!");
+		}
+		if (input.GetKeyRepeat(SDL_SCANCODE_RIGHT)) {
+			
+			angle -= speed * GetGameTime();
+			transform->SetLocalTransform(translation, scale, Quat::FromEulerXYX(0, angle * 0.01745329252, 0));
+			ConsoleLog("Moving Right!");
+		}
+		if (input.GetKeyRepeat(SDL_SCANCODE_LEFT)) {
+		
+			angle += speed * GetGameTime();
+			transform->SetLocalTransform(translation, scale, Quat::FromEulerXYX(0, angle * 0.01745329252, 0));
+			ConsoleLog("Moving Left!");	
+		}
 
 	}
-	else if (input.GetKeyRepeat(SDL_SCANCODE_RIGHT)) {
-
-	}
-	else if (input.GetKeyRepeat(SDL_SCANCODE_LEFT)) {
-
-	}*/
 }
 
 extern "C" __declspec(dllexport) const char* GetName() {
